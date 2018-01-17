1e365ee0ebc9f423083016e6668acb0d82abd14f
sultonyakbar <sultonyakbar@live.com>
2018-01-17 15:36:30 +0700

    Update elk.sh

--- elk.sh
+++ elk.sh
@@ -23,11 +23,12 @@ add-apt-repository -y ppa:webupd8team/java
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb http://packages.elastic.co/elasticsearch/${ELK_VERSION}/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-${ELK_VERSION}.list
 apt-get update
-apt-get install openjdk-8-jre-headless
+apt-get install -y openjdk-8-jre-headless
 
 ##############################################################################################
 ########## INSTALL ELASTICSEARCH
 ##############################################################################################
+apt-get -y install elasticsearch
 wget https://raw.githubusercontent.com/sultonyakbar/elk/elasticsearch/elasticsearch.yml -O /etc/elasticsearch/elasticsearch.yml
 systemctl restart elasticsearch
 systemctl daemon-reload
@@ -36,11 +37,25 @@ systemctl enable elasticsearch
 ##############################################################################################
 ########## INSTALL LOGSTASH
 ##############################################################################################
-apt-get install logstash
+apt-get -y install logstash
 wget https://raw.githubusercontent.com/sultonyakbar/elk/elasticsearch/elasticsearch.yml -O /etc/elasticsearch/elasticsearch.yml
+
 #Generate SSL Certificates
 mkdir -p /etc/pki/tls/certs
 mkdir /etc/pki/tls/private
+#nano /etc/ssl/openssl.cnf
+cd /etc/pki/tls
+openssl req -config /etc/ssl/openssl.cnf -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt
 
-
-
+#Configure Logstash
+touch /etc/logstash/conf.d/{input,filter,output}.conf
+wget https://raw.githubusercontent.com/sultonyakbar/elk/logstash/input.conf -O /etc/logstash/conf.d/input.conf
+wget https://raw.githubusercontent.com/sultonyakbar/elk/logstash/input.conf -O /etc/logstash/conf.d/filter.conf
+wget https://raw.githubusercontent.com/sultonyakbar/elk/logstash/input.conf -O /etc/logstash/conf.d/output.conf
+systemctl restart logstash
+systemctl enable logstash
+##############################################################################################
+########## INSTALL KIBANA
+##############################################################################################
+apt-get -y install kibana
+wget https://raw.githubusercontent.com/sultonyakbar/elk/kibana/kibana.yml -O /etc/kibana/kibana/kibana.yml
